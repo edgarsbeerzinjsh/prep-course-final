e565b4d37e9469c5f92ce2686f93673ccfeb8bbb
edgars.beerzinjsh <103140210+edgarsbeerzinjsh@users.noreply.github.com>
2022-12-11 23:35:58 +0200

    Tic Tac Toe finished

--- 00-tic-tac-toe/src/Game.test.ts
+++ 00-tic-tac-toe/src/Game.test.ts
@@ -13,4 +13,89 @@ describe("Tic-Tac-Toe", () => {
     expect(game.getWinner()).toBe("-");
     expect(game.isTie()).toBe(false);
   });
+  it("should be able to place X/O on cells", () => {
+    const game = new Game();
+
+    game.onClick(0);
+    game.onClick(1);
+    game.onClick(2);
+    expect(game.getCells()).toEqual([
+      "X", "O", "X",
+      "-", "-", "-",
+      "-", "-", "-"
+    ]);
+    expect(game.getTurn()).toBe("O");
+    expect(game.getWinner()).toBe("-");
+    expect(game.isTie()).toBe(false);
+  });
+  it("should not be able to place on filled cells", () => {
+    const game = new Game();
+
+    game.onClick(0);
+    game.onClick(0);
+
+    expect(game.getCells()).toEqual([
+      "X", "-", "-",
+      "-", "-", "-",
+      "-", "-", "-"
+    ]);
+    expect(game.getTurn()).toBe("O");
+    expect(game.getWinner()).toBe("-");
+    expect(game.isTie()).toBe(false);
+  });
+  it("should be able to see win status", () => {
+    const game = new Game();
+
+    game.onClick(0);
+    game.onClick(3);
+    game.onClick(1);
+    game.onClick(4);
+    game.onClick(2);
+
+    expect(game.getCells()).toEqual([
+      "X", "X", "X",
+      "O", "O", "-",
+      "-", "-", "-"
+    ]);
+    expect(game.getWinner()).toBe("X");
+    expect(game.isTie()).toBe(false);
+  });
+  it("should be able to see draw status", () => {
+    const game = new Game();
+
+    game.onClick(4);
+    game.onClick(8);
+    game.onClick(6);
+    game.onClick(2);
+    game.onClick(5);
+    game.onClick(3);
+    game.onClick(7);
+    game.onClick(1);
+    game.onClick(0);
+
+    expect(game.getCells()).toEqual([
+      "X", "O", "O",
+      "O", "X", "X",
+      "X", "X", "O"
+    ]);
+    expect(game.getWinner()).toBe("-");
+    expect(game.isTie()).toBe(true);
+  });
+  it("should be able to play again", () => {
+    const game = new Game();
+
+    game.onClick(4);
+    game.onClick(8);
+    game.onClick(6);
+    game.onClick(2);
+    game.onClick(5);
+    game.onClick(3);
+    game.onClick(7);
+    game.onClick(1);
+    game.onClick(0);
+    game.restart();
+    expect(game.getTurn()).toBe("X");
+    expect(game.getWinner()).toBe("-");
+    expect(game.isTie()).toBe(false);
+  });
 });
--- 00-tic-tac-toe/src/Game.ts
+++ 00-tic-tac-toe/src/Game.ts
@@ -1,27 +1,61 @@
 export type XO = "X" | "O" | "-";
 
 export class Game {
+  cells: XO[] = ["-", "-", "-", "-", "-", "-", "-", "-", "-"];
+  isTurnX: boolean = true
   getCells(): XO[] {
-    return ["X", "-", "-", "-", "-", "-", "-", "-", "-"];
+    return this.cells;
   }
 
   getTurn(): XO {
-    return "X";
+    return this.isTurnX ? "X" : "O";
   }
 
   getWinner(): XO {
+    const cell = this.cells;
+    //rows
+    if (cell[0] === cell[1] && cell[0] === cell[2]) {
+      return cell[0]
+    }
+    if (cell[3] === cell[4] && cell[4] === cell[5]) {
+      return cell[3]
+    }
+    if (cell[6] === cell[7] && cell[7] === cell[8]) {
+      return cell[6]
+    }
+    //columns
+    if (cell[0] === cell[3] && cell[3] === cell[6]) {
+      return cell[0]
+    }
+    if (cell[1] === cell[4] && cell[4] === cell[7]) {
+      return cell[1]
+    }
+    if (cell[2] === cell[5] && cell[5] === cell[8]) {
+      return cell[2]
+    }
+    //diagonal
+    if (cell[0] === cell[4] && cell[4] === cell[8]) {
+      return cell[0]
+    }
+    if (cell[6] === cell[4] && cell[4] === cell[2]) {
+      return cell[6]
+    }
     return "-";
   }
 
   isTie(): boolean {
-    return false;
+    return !this.cells.includes("-") && this.getWinner() === "-";
   }
 
   onClick(i: number): void {
-    console.log(`cell ${i} clicked`);
+    if (this.cells[i] === "-" && this.getWinner() === "-") {
+      this.cells[i] = this.isTurnX ? "X" : "O";
+      this.isTurnX = !this.isTurnX;
+    }
   }
 
   restart(): void {
-    console.log("restart called");
+    this.cells = new Array(9).fill("-");
+    this.isTurnX = true;
   }
 }
